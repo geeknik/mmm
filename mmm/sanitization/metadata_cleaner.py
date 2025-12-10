bc937fe71ee1a2954d836a514e5bf2a90f97cfc9
geeknik <geeknik@protonmail.ch>
2025-12-09 22:09:49 -0600

    Silence remaining pylint warnings for scripts/tests

--- mmm/sanitization/metadata_cleaner.py
+++ mmm/sanitization/metadata_cleaner.py
@@ -9,6 +9,7 @@ from typing import Dict, List, Any, Optional
 from mutagen import File as MutagenFile
 from mutagen.id3 import ID3, ID3NoHeaderError, TXXX, APIC, GEOB, PRIV
 from mutagen.mp3 import MP3
+from mutagen.wave import WAVE
 from pydub import AudioSegment
 
 
